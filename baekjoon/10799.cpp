92dab17964fa7c458f7c4be7ddd057ad24270554
jshForGit <mirolis6636@gmail.com>
2023-11-05 09:58:21 +0900

    10799 succeed

---
+++ baekjoon/10799.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cut_bars(string& Bars)
+{
+	int numOfBars=0;
+	int ret=0;
+	bool isLazer=false;
+
+	for(auto it=Bars.begin(); it!=Bars.end(); ++it)
+	{
+		if(*it=='(')
+		{
+			++numOfBars;
+			isLazer=true;
+		}
+		else //*it==')'
+		{
+			if(isLazer)
+			{
+				--numOfBars;
+				ret += numOfBars;	
+			}
+			else
+			{
+				++ret;
+				--numOfBars;
+			}
+			isLazer=false;
+		}
+	}
+	return ret;
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << cut_bars(s) << endl;
+}
