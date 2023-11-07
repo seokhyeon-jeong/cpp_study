ddb50ba562304429c9f74f1156f00ede718d95ac
jshForGit <mirolis6636@gmail.com>
2023-11-07 13:05:15 +0900

    17928 succeed

---
+++ baekjoon/17928.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+auto getInput(int n)
+{
+	vector<int> ret(n);
+	for(int i=0; i<n; ++i)
+		cin >> ret[i];
+	return ret;
+}
+
+auto NGE(const vector<int>& a)
+{
+	stack<int> stk;
+	vector<int> ret(a.size());
+
+	for(int i=a.size()-1; i>=0; --i)
+	{
+		while(!stk.empty() && stk.top()<a[i])
+			stk.pop();
+		if(stk.empty())
+			ret[i]=-1;
+		else
+			ret[i]=stk.top();
+		stk.push(a[i]);
+	}
+	return ret;
+}
+
+int main()
+{
+	vector<int> v{3,5,2,7};
+	auto ret = NGE(v);
+	for(auto ele : ret)
+		cout << ele << ' ';
+	cout << '\n';
+}
