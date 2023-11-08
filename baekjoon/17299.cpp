ae49cc7a05232b68e0cdefc8b91fa4ee643d1e7c
jshForGit <mirolis6636@gmail.com>
2023-11-08 13:28:52 +0900

    17299 finally succeed

--- baekjoon/17299.cpp
+++ baekjoon/17299.cpp
@@ -1,42 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int,int>> getArray(int N)
-{
-	map<int,int> m;
-	vector<pair<int,int>> ret;
+vector<int> cnt(1000001);
 
+vector<int> getArray(int N)
+{
+	vector<int> ret(N);
 	for(int i=0; i<N; ++i)
 	{
-		int tmp;
-		cin >> tmp;
-		if(m.find(tmp)==m.end())
-			m[tmp]=1;
-		else
-			++m[tmp];
-		ret.push_back({tmp,0});
+		cin >> ret[i];
+		++cnt[ret[i]];
 	}
-	for(auto& ele : ret)
-		ele.second = m[ele.first];
-
 	return ret;
 }
 
-vector<int> NGF(vector<pair<int,int>>& v)
+vector<int> NGF(vector<int>& A)
 {
-	stack<pair<int,int>> stk;
-	vector<int> ret(v.size());
+	stack<int> stk;
+	vector<int> ret(A.size());
 
-	for(int i=v.size()-1; i>=0; --i)
+	for(int i=A.size()-1; i>=0; --i)
 	{
-		while(!stk.empty() && stk.top().second<=v[i].second)
+		while(!stk.empty() && cnt[stk.top()]<=cnt[A[i]])
 			stk.pop();
-
+		
 		if(stk.empty())
 			ret[i]=-1;
 		else
-			ret[i]=stk.top().first;
-		stk.push(v[i]);
+			ret[i]=stk.top();
+
+		stk.push(A[i]);
 	}
 	return ret;
 }
@@ -45,8 +38,8 @@ int main()
 {
 	int N;
 	cin >> N;
-	auto v = getArray(N);
-	auto ret = NGF(v);
+	auto A = getArray(N);
+	auto ret = NGF(A);
 	for(auto ele : ret)
 		cout << ele << ' ';
 	cout << '\n';
