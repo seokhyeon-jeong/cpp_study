1168e080b0fdb95e7bb15262fcde21f0eedd7fca
jshForGit <mirolis6636@gmail.com>
2023-11-10 17:07:47 +0900

    2069 succeed

---
+++ baekjoon/2069.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned int uint;
+
+uint gcd(uint a, uint b)
+{
+	if(b==0)
+		return a;
+	return gcd(b, a%b);
+}
+
+uint lcm(uint a, uint b)
+{
+	return a/gcd(a,b)*b;
+}
+
+int main()
+{
+	uint a, b;
+	cin >> a >> b;
+	cout << gcd(a,b) << '\n' << lcm(a,b) << endl;
+}
