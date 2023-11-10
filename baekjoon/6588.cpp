ae6be22ae01b45f9efa987d2766e82827366e4bd
jshForGit <mirolis6636@gmail.com>
2023-11-10 21:20:37 +0900

    6588 hash version

--- baekjoon/6588.cpp
+++ baekjoon/6588.cpp
@@ -3,48 +3,45 @@ using namespace std;
 
 typedef unsigned int uint;
 
-static vector<uint> prime{2,3,5,7};
+static set<uint> prime;
 
 void pushPrime(uint target)
 {
 	bool isPrime=true;
 
-	for(uint n=prime.back()+1; n<=target; ++n)
+	for(uint n=*prime.rbegin()+2; n<=target; n+=2)
 	{
-		for(uint i=0; prime[i]*prime[i]<=n; ++i)
+		for(auto it=prime.begin(); (*it)*(*it)<=n; ++it)
 		{
-			if(n%prime[i]==0)
+			if(n%*it==0)
 			{
 				isPrime=false;
 				break;
 			}
 		}
 		if(isPrime)
-			prime.push_back(n);
+			prime.insert(n);
 		isPrime=true;
 	}
 }
 
 pair<int,int> Goldbach(uint n)
 {
-	if(prime.back()<n)
+	if(*prime.rbegin()<n)
 		pushPrime(n);
 
-	set<uint> hashTable;
 	uint maxDiff=0;
 	pair<uint,uint> ret{-1,-1};
-	for(uint i=0; i<prime.size(); ++i)
+	for(auto it=prime.begin();it!=prime.end(); ++it)
 	{
-		uint target=n-prime[i];
-		auto found=hashTable.find(target);
-		if(found==hashTable.end())
-			hashTable.insert(prime[i]);
-		else
+		uint target=n-*it;
+		auto found=prime.find(target);
+		if(found!=prime.end())
 		{
-			if(abs(static_cast<int>(*found-prime[i]))>maxDiff)
+			if(abs(static_cast<int>(*found-*it))>maxDiff)
 			{
-				ret = make_pair(*found,prime[i]);
-				maxDiff=abs(static_cast<int>(*found-prime[i]));
+				ret = make_pair(*found,*it);
+				maxDiff=abs(static_cast<int>(*found-*it));
 			}
 		}
 	}
@@ -54,6 +51,10 @@ pair<int,int> Goldbach(uint n)
 int main()
 {
 	uint n;
+	prime.insert(2);
+	prime.insert(3);
+	prime.insert(5);
+	prime.insert(7);
 	while(true)
 	{
 		cin >> n;
