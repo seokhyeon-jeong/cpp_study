09eefb0eccd486fa992b7e490c4928a0d4411e8e
jshForGit <mirolis6636@gmail.com>
2023-11-10 17:48:23 +0900

    1929 3try

--- baekjoon/1929.cpp
+++ baekjoon/1929.cpp
@@ -5,17 +5,21 @@ typedef unsigned int uint;
 
 vector<uint> prime(uint M, uint N)
 {
-	vector<uint> ret{2};
+	vector<uint> ret{2,3};
 	vector<uint>::iterator it;
-	for(uint i=3; i*i<=N; i+=2)
+	bool is_prime=true;	
+	for(uint i=5; i<=N; i+=2)
 	{
-		for(it=ret.begin();it!=ret.end();++it)
-			if(i%*it==0)
+		for(it=ret.begin(); (*it)*(*it)<=i; ++it)
+			if(i % (*it) == 0)
+			{
+				is_prime=false;
 				break;
-		if(it==ret.end())
+			}
+		if(is_prime)
 			ret.push_back(i);
+		is_prime = true;
 	}
-	
 	auto start=lower_bound(ret.begin(), ret.end(), M);
 
 	return vector<uint>{start,ret.end()};
