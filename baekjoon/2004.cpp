45ee371878171371e2693233efabc692a2e48953
jshForGit <mirolis6636@gmail.com>
2023-11-17 16:01:06 +0900

    2004.cpp 1try

---
+++ baekjoon/2004.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int zeros(const int n)
+{
+	int ret=0;
+
+	for(int i=5; i<=n; i*=5)
+	{
+		ret += n/i;
+	}
+	return ret;
+}
+
+int main()
+{
+	int n,m;
+	cin >> n >> m;
+	cout << zeros(m) << endl;
+}
