96c613ebf45e60ab17b4ac578b886480c6d816c3
jshForGit <mirolis6636@gmail.com>
2023-12-04 12:09:51 +0900

    알고분

--- baekjoon/6588.cpp
+++ baekjoon/6588.cpp
@@ -1,60 +1,45 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define INPUT_LIMIT 1000000
 
-static vector<int> indexedPrime(INPUT_LIMIT+1,0);
-static vector<int> prime;
+#define MAX_N 1000000
 
-void generatePrime()
+std::vector<bool> isPrime(MAX_N, true);
+
+void eratosthenes(const int n=MAX_N)
 {
-	prime.push_back(2);
-	prime.push_back(3);
-	indexedPrime[2]=indexedPrime[3]=1;
-	
+	isPrime[0]=isPrime[1]=false;
 
-	for(size_t num=5; num<=INPUT_LIMIT; num+=2)
+	auto sqrtn = int(std::sqrt(n));
+
+	for(auto i=3; i<=sqrtn; i+=2)
 	{
-		bool isPrime=true;
-		for(size_t i=0; i<prime.size() && prime[i]*prime[i]<=num; ++i)
+		if(isPrime[i])
 		{
-			if(num % prime[i] == 0)
-			{
-				isPrime=false;
-				break;
-			}
-		}
-		if(isPrime)
-		{
-			prime.push_back(num);
-			indexedPrime[num]=1;
+			for(auto j=i*i; j<=n; j+=i)
+				isPrime[j]=false;
 		}
 	}
 }
 
 void goldbach(const int n)
 {
-	for(size_t i=1; i<prime.size(); ++i)
+	for(auto i=3; i<=n; i+=2)
 	{
-		auto ele=prime[i];
-		if(ele>=n)
+		if(isPrime[i] && isPrime[n-i])
 		{
-			cout << "Goldbach's conjecture is wrong.\n";
-			break;
-		}
-		if(indexedPrime[ele]==1 && indexedPrime[n-ele]==1)
-		{
-			cout << n << " = " << ele << " + " << n-ele << '\n';
-			break;
+			std::cout << n << " = " << i << " + " << n-i << '\n';
+			return;
 		}
 	}
+	std::cout << "Goldbach's conjecture is wrong.\n";
 }
+
 int main()
 {
-	generatePrime();
-	int n;
+	eratosthenes();
 	while(true)
 	{
-		cin >> n;
+		int n;
+		std::cin >> n;
 		if(n==0)
 			break;
 		goldbach(n);
