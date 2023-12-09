7a1840841ec265d4bf052db0bdb2dbe112aed69f
jshForGit <mirolis6636@gmail.com>
2023-12-09 23:43:39 +0900

    eratosthnes, bitmask

--- baekjoon/6588.cpp
+++ baekjoon/6588.cpp
@@ -1,51 +1,52 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-#define MAX_N 1000001
+#define MAX_N 1000*1000
 
-std::vector<bool> isPrime(MAX_N, true);
-std::vector<int> prime;
+unsigned char sieve[(MAX_N+7)/8];
 
-void eratosthenes(const int n=MAX_N)
-{
-	isPrime[0]=isPrime[1]=false;
-
-	auto sqrtn = int(std::sqrt(n));
-
-	for(auto i=2; i<=sqrtn; ++i)
-	{
-		if(isPrime[i])
-		{
-			prime.push_back(i);
-			for(auto j=i*i; j<=n; j+=i)
-				isPrime[j]=false;
-		}
-	}
+inline bool isPrime(int k){
+	return sieve[k>>3] & (1<<(k&7));
 }
 
-void goldbach(const int n)
-{
-	for(auto ele : prime)
-	{
-		if(ele>=n)
-			break;
-		if(isPrime[n-ele])
-		{
-			std::cout << n << " = " << ele << " + " << n-ele << '\n';
-			return;
+inline void setComposite(int k){
+	sieve[k>>3] &= ~(1<<(k&7));
+}
+
+void eratosthenes(){
+	memset(sieve, 0xff, sizeof(sieve));
+	setComposite(0);
+	setComposite(1);
+
+	int sqrtn=int(sqrt(MAX_N));
+	for(auto i=2; i<sqrtn; ++i){
+		if(isPrime(i)){
+			for(auto j=i*i; j<MAX_N; j+=i){
+				setComposite(j);
+			}
 		}
 	}
-	std::cout << "Goldbach's conjecture is wrong.\n";
 }
 
 int main()
 {
 	eratosthenes();
-	while(true)
-	{
-		int n;
-		std::cin >> n;
+
+	int n;
+	while(true){
+		cin >> n;
 		if(n==0)
 			break;
-		goldbach(n);
+		int halfn=(n>>1);
+		for(auto i=3; i<=halfn; i+=2){
+			if(isPrime(i) && isPrime(n-i)){
+				printf("%d = %d + %d\n",n,i,n-i);
+				n=0;
+				break;
+			}
+		}
+		if(n!=0)
+			puts("Goldbach's conjecture is wrong.");
 	}
 }
+
