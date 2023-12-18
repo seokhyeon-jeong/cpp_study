9e704bfc7e89b4f33550cb0ec88573e7df8d4263
jshForGit <mirolis6636@gmail.com>
2023-12-18 12:56:20 +0900

    dp 11727

---
+++ baekjoon/11727.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX_N 1000
+#define MOD 10007
+
+int cache[MAX_N+1];
+
+int tiling(int n){
+	if(n<=1)
+		return 1;
+	int& ret=cache[n];
+	if(ret!=-1)
+		return ret;
+	return ret=(tiling(n-1)+2*tiling(n-2))%MOD;
+}
+
+int main()
+{
+	memset(cache, -1, sizeof(cache));
+	int n;
+	cin >> n;
+	cout << tiling(n) << endl;
+}
