52f57129460bbf6481675203630d72948268fb25
jshForGit <mirolis6636@gmail.com>
2024-01-07 21:02:35 +0900

    1149 dp

--- baekjoon/1149.cpp
+++ baekjoon/1149.cpp
@@ -1,19 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int house[1001][3], cost[3];
+int house[1001][3], N;
+
+int solution(const int n){
+	scanf("%d %d %d",&house[0][0], &house[0][1], &house[0][2]);
+
+	for(int i=1; i<n; ++i){
+		scanf("%d %d %d",&house[i][0], &house[i][1], &house[i][2]);
+
+		house[i][0] += min(house[i-1][1],house[i-1][2]);
+		house[i][1] += min(house[i-1][0],house[i-1][2]);
+		house[i][2] += min(house[i-1][0],house[i-1][1]);
+	}
+	
+	return min(house[n-1][0], min(house[n-1][1],house[n-1][2]));
+}
 
 int main(void){
-	int N;
-	scanf("%d",&N);
-	
-	for(int i=1; i<=N; ++i){
-		scanf("%d %d %d",&cost[0],&cost[1],&cost[2]);
-		house[i][0]=min(house[i-1][1],house[i-1][2])+cost[0];
-		house[i][1]=min(house[i-1][0],house[i-1][1])+cost[1];
-		house[i][2]=min(house[i-1][1],house[i-1][2])+cost[2];
-	}
+	scanf("%d", &N);
 
-	N=min(house[N][0],min(house[N][1],house[N][2]));
-	printf("%d\n",N);
+	int ret=solution(N);
+	cout << ret << endl;
 }
+
