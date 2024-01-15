6a1e072ff2980501e77c24d72c316e8855591af2
jshForGit <mirolis6636@gmail.com>
2024-01-15 15:04:30 +0900

    11054 dp

--- baekjoon/11054.cpp
+++ baekjoon/11054.cpp
@@ -1,39 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, A[1000], cache[1000];
+int N, A[1000], lisCache[1001], ldsCache[1001];
 
-void lds(void){
-	cache[N-1]=1;
-
-	for(int idx=N-2; idx>=0; --idx){
-		cache[idx]=1;
-		for(int next=idx+1; next<N; ++next){
-			if(A[idx]>A[next])
-				cache[idx]=max(cache[idx], cache[next]+1);
+void lis(void){
+	lisCache[0]=1;
+	for(int start=1; start<N; ++start){
+		int& ret=lisCache[start];
+		ret=1;
+		for(int next=start-1; next>=0; --next){
+			if(A[start]>A[next])
+				ret=max(ret, lisCache[next]+1);
 		}
 	}
 }
-
-int bitonic(int start){
-	int ret=1;
-	for(int next=start+1; next<N; ++next){
-		if(A[start]<A[next])
-			ret=max(ret,bitonic(next)+1);
-		else if(A[start]>A[next])
-			ret=max(ret,cache[next]+1);
-		else
-			;
+void lds(void){
+	ldsCache[N-1]=1;
+	for(int start=N-2; start>=0; --start){
+		int& ret=ldsCache[start];
+		ret=1;
+		for(int next=start+1; next<N; ++next){
+			if(A[start]>A[next])
+				ret=max(ret, ldsCache[next]+1);
+		}
 	}
-	return ret;
 }
 int main(void){
 	cin >> N;
 	for(int i=0; i<N; ++i)
 		scanf("%d", &A[i]);
+
+	lis();
 	lds();
-	int ret=1;
-	for(int begin=0; begin<N; ++begin)
-		ret=max(ret, bitonic(begin));
-	cout << ret << endl;
+
+	int ret=-1;
+	for(int i=0; i<N; ++i){
+		lisCache[i] += ldsCache[i];
+		if(lisCache[i]>ret)
+			ret=lisCache[i];
+	}
+	cout << --ret << endl;
+	return 0;
 }
+
