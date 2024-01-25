48f819955865d5c24d61204f088402a2cb789098
jshForGit <mirolis6636@gmail.com>
2024-01-25 10:52:28 +0900

    15664

---
+++ baekjoon/15664.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,n) for(auto i=0; i<(n); ++i)
+
+int N, M, input[2][8], buf[8];
+
+void solution(int start,int depth){
+	if(depth==M){
+		FOR(i,depth)
+			cout << buf[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	int flag=0;	
+	for(int i=start; i<N; ++i){
+		if(input[1][i]==-1)
+			continue;
+		if(input[0][i]==flag)
+			continue;
+		input[1][i]=-1;
+		flag=input[0][i];
+		buf[depth]=input[0][i];
+		solution(i, depth+1);
+		input[1][i]=0;
+	}
+}
+
+int main(void){
+	cin >> N >> M;
+	FOR(i,N)
+		cin >> input[0][i];
+	sort(input[0], input[0]+N);
+	solution(0,0);
+}
+
