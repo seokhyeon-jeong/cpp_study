fe2dcbcab3c2577b24906013fd5e4455b95c94a9
jshForGit <mirolis6636@gmail.com>
2024-01-25 14:32:16 +0900

    10972

---
+++ baekjoon/10972.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,n) for(auto i=0; i<(n); ++i)
+
+int N, a[10000];
+
+bool solution(void){
+	int i=N-1;
+	while(i>=0 && a[i-1]>a[i])
+		--i;
+	if(i==0)
+		return false;
+
+	int j=N-1;
+	while(a[i-1]>a[j])
+		--j;
+	swap(a[i-1],a[j]);
+	for(int left=i, right=N-1; left!=right; ++left,--right)
+		swap(a[left],a[right]);
+	return true;
+}
+
+int main(void){
+	cin >> N;
+	FOR(i,N)
+		scanf(" %d", &a[i]);
+	if(solution())
+		FOR(i,N)
+			printf("%d ",a[i]);
+	else
+		putchar(-1);
+}
+
+
