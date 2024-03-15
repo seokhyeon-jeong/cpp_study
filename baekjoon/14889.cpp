01ddc75f7f7a9b8e8b4088217d288474f00a8eb5
jshForGit <mirolis6636@gmail.com>
2024-03-15 16:09:46 +0900

    14889

---
+++ baekjoon/14889.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,n) for(auto i=0; i<(n); ++i)
+
+int N, S[20][20];
+int members[20], ret=987654321;
+void solution(int depth){
+    if(depth==N/2){
+        int startTeam=0, linkTeam=0;
+        FOR(start,N){
+            FOR(link,N){
+                if(members[start]==1 && members[link]==1)
+                    startTeam += S[start][link];
+                if(members[start]==0 && members[link]==0)
+                    linkTeam += S[start][link];
+            }
+        }
+        ret = min(ret, abs(startTeam-linkTeam));
+        return;
+    }
+    FOR(i,N){
+        if(members[i]==1)
+            continue;
+        members[i]=1;
+        solution(depth+1);
+        members[i]=0;
+    }
+}
+int main(void){
+    cin >> N;
+    FOR(i,N)
+        FOR(j,N)
+            cin >> S[i][j];
+    solution(0);
+    cout << ret << endl;
+}
